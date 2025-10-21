7338c708650f30111cc1b08d343f2d1444d7752b
Data_Structures_and-algorithm <ijazhamza2005@gmail.com>
2025-10-22 01:08:24 +0500

    LINKlist

--- Linklist/singly_linklist/insertion_at_specific.cpp
+++ Linklist/singly_linklist/insertion_at_specific.cpp
@@ -35,7 +35,26 @@ void display(){
     }
     cout<<"NULL"<<endl;
 }
-
+void insert_at_specific_pos(int value,int pos){
+    if(pos==1){
+        insert_at_start(value);
+        return ;
+    }
+    node* newnode=new node();
+    node*temp=head;
+    newnode->data=value;
+    for(int i=1;i<pos-1&&temp!=NULL;i++){
+        temp=temp->next;
+    }
+    if(temp==NULL){
+        cout<<"INVALID POSITION "<<endl;
+        cout<<"DELETE "<<newnode->data<<endl;
+        delete newnode;
+        return;
+    }
+    newnode->next=temp->next;
+    temp->next=newnode;
+}
 
 
 int main(){
@@ -51,6 +70,11 @@ int main(){
     cout<<endl;
     cout<<"Now doing Insertion at end : "<<endl;
     insert_at_end(50);
+    display();
+    cout<<endl;
+    cout<<"Now doing Insertion at specific: "<<endl;
+    insert_at_specific_pos(9090,4);
+    // insert_at_specific_pos(90900,111);
     display();
     return 0;
 }
