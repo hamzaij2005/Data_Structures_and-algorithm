cd68ec1a62b9c829b0c6150a3cf804ca90632dfd
Data_Structures_and-algorithm <ijazhamza2005@gmail.com>
2025-10-22 07:59:37 +0500

    LINKlist

---
+++ Linklist/doubly_linklist/insert_at_start.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+        int data;
+        node* next;
+        node* pre;
+};
+node* head=NULL;
+
+void insert(int value){
+    node* newnode=new node;
+    newnode->data=value;
+    newnode->next=head;
+    if(head!=NULL){
+        head->pre=newnode;
+    }
+    head=newnode;
+}
+void display(){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" <-> ";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+int main(){
+    insert(100);
+    insert(200);
+    insert(300);
+    insert(400);
+    insert(500);
+    display();
+    return 0;
+}
